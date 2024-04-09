b50d3392874feb24de3707940f5af28829c37197
SauradipGhosh <sauradip96ghosh@gmail.com>
2024-04-09 23:10:44 +0530

    Demo shema added

--- index.js
+++ index.js
@@ -1,30 +1,71 @@
+require("dotenv").config(); // environment variable
+// require packages
 const express = require("express");
-var cors = require("cors");
-const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
-require("dotenv").config();
+// initialise express
+const mongoose = require("mongoose");
 
 const app = express();
 const port = 8000;
 
 app.use(cors());
 app.options("*", cors());
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
+//  mondodb connect
+mongoose.connect(process.env.MONGODB_URL)
+   .then(() => {
+      console.log("connected"); // Success!
+   })
+   .catch((err) => {
+      console.log("err", err);
+   });
+// create a schema
+const studentSchema = new mongoose.Schema({
+  roll_no: Number,
+  name: String,
+  year: Number,
+  subjects: [String]
+});
 
-mongoose
-  .connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("connected"); // Success!
-  })
-  .catch((err) => {
-    console.log("err", err);
-  });
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+// create a model with studentSchema
+const Student = mongoose.model("Student", studentSchema);
+// create a schema
+const stud = new Student({
+   roll_no: 1001,
+   name: "Madison Hyde",
+   year: 3,
+   subjects: ["DBMS", "OS", "Graph Theory", "Internet Programming"],
+});
+stud.save().then(
+   () => console.log("One entry added"),
+   (err) => console.log(err)
+);
+
+
+app.get("/", async(req, res) => {
+  let result = await Student.find();
+  res.status(200).json(result);
+
+  try{
+    let result = await Student.find();
+    res.status(200).json(result);
+  } catch (error){
+    res.status(500).json(error);
+  }
+  //  Student.find({}, (err, found) => {
+  //     if (!err) {
+  //        res.send(found);
+  //     }
+  //     console.log(err);
+  //     res.send("Some error occured!");
+  //  }).catch((err) => console.log("Error occured, " + err));
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+   console.log(`Example app listening on port ${port}`);
 });
